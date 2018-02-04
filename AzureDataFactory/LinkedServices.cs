249c6d1fd0715213ef37e3ed3ea8f9daf1b9b42b
adrabowicz <bambus.ca@gmail.com>
2018-02-04 09:35:32 -0800

    CreateHttpLinkedServiceResource

--- AzureDataFactory/LinkedServices.cs
+++ AzureDataFactory/LinkedServices.cs
@@ -13,13 +13,28 @@ namespace ADFv2QuickStart
     {
         public static void CreateStorageLinkedServiceResource(DataFactoryManagementClient client, string dataFactoryName, string linkedServiceName)
         {
-
-            Console.WriteLine("Creating linked service " + linkedServiceName + "...");
             var connectionString = "DefaultEndpointsProtocol=https;AccountName=" + Config.StorageAccount + ";AccountKey=" + Config.StorageKey;
             var properties = new AzureStorageLinkedService
             {
                 ConnectionString = new SecureString(connectionString)
             };
+            CreateLinkedServiceResource(client, dataFactoryName, linkedServiceName, properties);
+        }
+
+        public static void CreateHttpLinkedServiceResource(DataFactoryManagementClient client, string dataFactoryName, string linkedServiceName)
+        {
+            var properties = new HttpLinkedService
+            {
+                AuthenticationType = "Anonymous",
+                Url = "",   // base url of the web server
+                EnableServerCertificateValidation = false
+            };
+            CreateLinkedServiceResource(client, dataFactoryName, linkedServiceName, properties);
+        }
+
+        private static void CreateLinkedServiceResource(DataFactoryManagementClient client, string dataFactoryName, string linkedServiceName, LinkedService properties)
+        {
+            Console.WriteLine("Creating linked service " + linkedServiceName + "...");
             var linkedServiceResource = new LinkedServiceResource(properties);
             client.LinkedServices.CreateOrUpdate(Config.ResourceGroup, dataFactoryName, linkedServiceName, linkedServiceResource);
             Console.WriteLine(SafeJsonConvert.SerializeObject(linkedServiceResource, client.SerializationSettings));
